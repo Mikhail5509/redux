26183367a700064dac91e81d230eab06b66a4a98
Mikhail Ignatov <ignatov.m55@gmail.com>
2025-04-01 11:04:10 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EditableList from "./EditableList";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Редактируемый список</h1>
+      <EditableList />
+    </div>
+  );
+};
+
+export default App;
